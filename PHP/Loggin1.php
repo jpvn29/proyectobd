cecafa19022196b2a315dbf81c7fe0ad0d92b766
jpvn29 <JPVN@JPVN.local>
2016-04-22 18:56:41 -0500

    nuevo

---
+++ PHP/Loggin1.php
@@ -0,0 +1,49 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="proyecto";
+$message = "Passwor incorrecto";
+$message1 = "Passwor correcto";
+
+
+$db_connection = mysql_connect($db_host, $db_user, $db_password);
+$database = mysql_select_db($db_name, $db_connection);
+
+
+
+$mail = $_POST['email'];
+$pass = $_POST['contraseña'];
+
+
+$req = (strlen($pass)*strlen($mail)) or die ("No se llenaron los campos");
+
+
+
+
+
+$consulta= "SELECT PW from Usuario where email = '$mail' ";
+
+$resultado = mysql_query($consulta,$db_connection);
+$result = mysql_fetch_array($resultado);
+$fina=$result['PW'];
+
+
+
+
+if ($fina == $pass)
+{
+
+ echo "<script type='text/javascript'> window.location.href = '../principal.html'</script>";
+
+}
+else
+{
+echo "<script type='text/javascript'>alert('$message');</script>";
+echo "<script type='text/javascript'> window.location.href = '../loggin.html'</script>";
+
+
+}	
+
+		
+?>
---
+++ PHP/registro.php
@@ -0,0 +1,35 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="proyecto";
+$message = "password no coinciden";
+
+
+$db_connection = mysql_connect($db_host, $db_user, $db_password);
+$database = mysql_select_db($db_name, $db_connection);
+
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$pass = $_POST['contraseña'];
+$rpass = $_POST['rcontraseña'];
+
+$req = (strlen($nombre)*strlen($mail)) or die ("No se llenaron los campos");
+
+if($pass != $rpass){
+	echo "<script type='text/javascript'>alert('$message');</script>";
+
+}
+elseif ($pass == $rpass)
+{
+//encriptar contraseña
+$passUsuario = md5($pass);
+
+$consulta="INSERT INTO usuario (Nombre,Email,PW) VALUES ('$nombre','$mail','$pass')";
+
+$resultado = mysql_query($consulta,$db_connection);
+}
+
+		
+?>
